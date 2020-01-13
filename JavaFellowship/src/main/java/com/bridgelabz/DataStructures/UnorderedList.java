e214558a87735c31ae6068c98f8e5c1b382e7207
tkoteswararao131997 <tkoteswararao131997@gmail.com>
2020-01-13 18:52:09 +0530

    [koti]unorderedlist

--- JavaFellowship/src/main/java/com/bridgelabz/DataStructures/UnorderedList.java
+++ JavaFellowship/src/main/java/com/bridgelabz/DataStructures/UnorderedList.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.DataStructures;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
--- JavaFellowship/src/main/java/com/bridgelabz/utility/UtilityLinkedList.java
+++ JavaFellowship/src/main/java/com/bridgelabz/utility/UtilityLinkedList.java
@@ -1,8 +1,4 @@
 package com.bridgelabz.utility;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Scanner;
 public class UtilityLinkedList 
 {
 	UtilityLinkedList head;
