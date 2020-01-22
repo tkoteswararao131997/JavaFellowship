ff5fa75585ce8885780910d57257ee74020eae49
tkoteswararao131997 <tkoteswararao131997@gmail.com>
2020-01-22 19:23:31 +0530

    [koti]new

---
+++ StockAccountmanagement/src/main/java/com/bridgelabz/module/DataAssign.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.module;
+
+public class DataAssign
+{
+	private String stockname;
+	private int numberofshares;
+	private long shareprice;
+	public String getStockname() {
+		return stockname;
+	}
+	public void setStockname(String stockname) {
+		this.stockname = stockname;
+	}
+	public long getShareprice() {
+		return shareprice;
+	}
+	public void setShareprice(long shareprice) {
+		this.shareprice = shareprice;
+	}
+	public int getNumberofshare() {
+		return numberofshares;
+	}
+	public void setNumberofshare(int numberofshare) {
+		this.numberofshares = numberofshare;
+	}
+	public String toString()
+	{
+		return "stockname:"+stockname+",numberofshares:"+numberofshares+",shareprice:"+shareprice;
+	}
+}
