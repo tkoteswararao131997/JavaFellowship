43ed6a80a5a76a90b84cdac466d1116886f0f709
tkoteswararao131997 <tkoteswararao131997@gmail.com>
2020-01-27 11:00:53 +0530

    enum modified,completed

--- DesignPatterns/src/main/java/com/bridgelabz/singleton/EnumSingleton.java
+++ DesignPatterns/src/main/java/com/bridgelabz/singleton/EnumSingleton.java
@@ -7,11 +7,19 @@ public enum EnumSingleton
 //	{
 //		System.out.println("hello");
 //	}
-	public void someMeth(String str)
+//	int i;
+	public  void someMeth()
 	{
-		//write something
+//		System.out.println(i);
+		//do something
 	}
-	public static void main(String[] args) {
-		System.out.println(EnumSingleton.INSTANCE);
-	}
+//	public static void main(String[] args) 
+//	{
+//		EnumSingleton obj=EnumSingleton.INSTANCE;
+//		obj.i=10;
+//		obj.someMeth();
+//		EnumSingleton obj1=EnumSingleton.INSTANCE;
+//		obj1.i=20;
+//		obj.someMeth();
+//	}
 }
